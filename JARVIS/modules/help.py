4b1422d070b2713a0b025a204de55f4a1083e077
CenzoGod <162982407+CenzoGod@users.noreply.github.com>
2024-03-24 02:01:55 +0530

    Update help.py

--- JARVIS/modules/help.py
+++ JARVIS/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ 𝕽𝖊𝖆𝖕𝖊𝖗 ʙᴏᴛs ʜᴇʟᴘ ᴍᴇɴᴜ ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @JARVIS_V2**"
+HELP_STRING = f"★ 𝕽𝖊𝖆𝖕𝖊𝖗 ʙᴏᴛs ʜᴇʟᴘ ᴍᴇɴᴜ ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @ll_You_Are_Next_ll**"
 
 HELP_BUTTON = [
     [
