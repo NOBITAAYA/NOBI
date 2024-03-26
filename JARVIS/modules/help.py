ba857237a5c25fa1eeb35b76bb4072f70d45d21b
CenzoGod <162982407+CenzoGod@users.noreply.github.com>
2024-03-26 10:02:08 +0530

    Update help.py

--- JARVIS/modules/help.py
+++ JARVIS/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ 𝕽𝖊𝖆𝖕𝖊𝖗 ʙᴏᴛs ʜᴇʟᴘ ᴍᴇɴᴜ ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @ll_You_Are_Next_ll**"
+HELP_STRING = f"★ 🏴‍☠️𝕽𝖊𝖆𝖕𝖊𝖗🏴‍☠️ ʙᴏᴛs ʜᴇʟᴘ ᴍᴇɴᴜ ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @Reaperx5**"
 
 HELP_BUTTON = [
     [
@@ -14,8 +14,8 @@ HELP_BUTTON = [
       Button.inline("• ᴇxᴛʀᴀ •", data="extra")
     ],
     [
-      Button.url("• OWNER •", "https://t.me/ll_You_Are_Next_ll"),
-      Button.url("• SUPPORT •", "https://t.me/Fakecenzo_69")
+      Button.url("• OWNER •", "https://t.me/Reaperx5"),
+      Button.url("• SUPPORT •", "https://t.me/REAPER_SUPPORT")
     ]
   ]
 
@@ -60,7 +60,7 @@ extra_msg = f"""
   2) {hl}leave : Type in the Group bot will auto leave that group
 
 
-**© @ll_You_Are_Next_ll**
+**© @Reaperx5**
 """
 
                  
@@ -92,7 +92,7 @@ raid_msg = f"""
   2) {hl}craid <count> <reply to user>
 
 
-**© @ll_You_Are_Next_ll**
+**© @Reaperx5**
 """
 
 spam_msg = f"""
@@ -109,7 +109,7 @@ spam_msg = f"""
   1) {hl}hang <counter>
 
 
-** © @ll_You_Are_Next_ll**
+** © @Reaperx5**
 """                     
            
            
@@ -136,13 +136,13 @@ async def helpback(event):
                 Button.inline("• ᴇxᴛʀᴀ •", data="extra")
               ],
               [
-                Button.url("• OWNER •", "https://t.me/ll_You_Are_Next_ll"),
-                Button.url("• SUPPORT •", "https://t.me/Fakecenzo_69")
+                Button.url("• OWNER •", "https://t.me/Reaperx5"),
+                Button.url("• SUPPORT •", "https://t.me/REAPER_SUPPORT")
               ]
             ]
           )
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𝕽𝖊𝖆𝖕𝖊𝖗 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @ll_You_Are_Next_ll", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𝐑𝖊𝖆𝖕𝖊𝖗ㅤᯤ⁹⁹⁹⁺ ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 💀 @Reaperx5", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"spam"))
@@ -161,7 +161,7 @@ async def help_spam(event):
               buttons=[[Button.inline("< Back", data="help_back"),],],
               ) 
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𝕽𝖊𝖆𝖕𝖊𝖗 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @ll_You_Are_Next_ll", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𝐑𝖊𝖆𝖕𝖊𝖗ㅤᯤ⁹⁹⁹⁺ ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 💀 @Reaperx5", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"raid"))
@@ -180,7 +180,7 @@ async def help_raid(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
           )
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𝕽𝖊𝖆𝖕𝖊𝖗 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @ll_You_Are_Next_ll", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𝐑𝖊𝖆𝖕𝖊𝖗ㅤᯤ⁹⁹⁹⁺ ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 💀 @Reaperx5", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"extra"))
@@ -199,5 +199,5 @@ async def help_extra(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
             )
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𝕽𝖊𝖆𝖕𝖊𝖗 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @ll_You_Are_Next_ll", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𝐑𝖊𝖆𝖕𝖊𝖗ㅤᯤ⁹⁹⁹⁺ ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 💀 @Reaperx5", cache_time=0, alert=True)
 
